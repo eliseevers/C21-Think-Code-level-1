374744542642988fedacc73c5cdd3cfa0056a946
eliseevers <2155620@talnet.nl>
2021-10-27 14:42:15 +0200

    eindopdracht thinkcode

---
+++ eindopdracht.py
@@ -0,0 +1,9 @@
+def favoriete_films(name):
+    print("een van mijn favoriete films is", name)
+
+favoriete_films("James bond casino royale")
+favoriete_films("Brirds of prey")
+favoriete_films("Suicide squad")
+
+awnser=input("wat is jou favoriete film?")
+print(awnser ,"is inderdaad een leuke film")
