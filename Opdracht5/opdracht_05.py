37daec8dae5a4594fd16a2d35f56a8d5efa93057
eliseevers <2155620@talnet.nl>
2021-10-27 13:34:47 +0200

    opdracht 5 think code

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,12 @@
+def favorite_city(name):
+    print ("my favorite city is", name)
+
+favorite_city("Paris, France")
+favorite_city("Rome, Italy")
+favorite_city("Sevilla, Spain")
+
+
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
+
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
