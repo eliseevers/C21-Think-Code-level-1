9d268f2b1d0e68c1d161859df49a7d9743966af2
eliseevers <2155620@talnet.nl>
2021-11-05 10:13:33 +0100

    eindopdracht python

--- eindopdracht.py
+++ eindopdracht.py
@@ -7,3 +7,14 @@ favoriete_films("Suicide squad")
 
 awnser=input("wat is jou favoriete film?")
 print(awnser ,"is inderdaad een leuke film")
+
+print("even checken of je heb opgelet")
+
+awnser = input("wat is mijn eerste favorite film?")
+
+if awnser == " james bond casino royale":
+    print(True)
+else:
+    print(False)
+
+print("Leuk dat je meedeed.")
